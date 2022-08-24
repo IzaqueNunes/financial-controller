ade8c5106636efa79aacd4604449522685e315c3
IzaqueNunes <izaquenunes560@gmail.com>
2022-08-24 10:35:57 -0300

    Created Header

---
+++ src/App.styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Header = styled.div`
+  display: flex;
+  background-color: #0b132b;
+  height: 150px;
+  justify-content: center;
+  padding-top: 30px;
+`;
+
+export const HeaderText = styled.h1`
+  margin: 0;
+  padding: 0;
+  color: #fff;
+  font-family: "Poppins", sans-serif;
+`;
+
+export const Body = styled.div`
+  margin: auto;
+  max-width: 980px;
+  margin-bottom: 50px;
+`;
